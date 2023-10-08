6040a070027a462cc5616104504f269265a9518d
Tomas Jansson <tomas.jansson@gmail.com>
2023-10-09 00:24:11 +0200

    Start transition to swr

--- app/resource-groups/resource-group-list.tsx
+++ app/resource-groups/resource-group-list.tsx
@@ -1,22 +1,48 @@
+"use client";
 import ResourceRow from "@/components/resource-row";
 import Link from "next/link";
 import React from "react";
 import { formatDistance } from "date-fns";
 import { getStacks } from "@/lib/pulumi-client";
 import DeleteForm from "@/components/delete-form";
+import useSWR from "swr";
+import { ResourceItem } from "@/lib/types";
+import { Skeleton } from "@/components/ui/skeleton";
 
 type Props = {};
 
-const ResourceGroupList = async (props: Props) => {
-  const stacks = await getStacks("resource-groups");
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-  console.log(process.env.ARM_CLIENT_ID);
-  console.log(process.env.ARM_CLIENT_SECRET);
-  console.log(process.env.ARM_SUBSCRIPTION_ID);
-  console.log(process.env.ARM_TENANT_ID);
+const ListSkeleton = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+      <Skeleton className="w-full h-16" />
+    </div>
+  );
+};
+
+const ResourceGroupList = (props: Props) => {
+  const {
+    data: stacks,
+    error,
+    isLoading,
+  } = useSWR<ResourceItem[]>("/api/resource-groups", fetcher);
+
+  // const stacks = await getStacks("resource-groups");
 
   console.log(stacks);
-  return (
+  if (isLoading) {
+    return <ListSkeleton />;
+  }
+  return stacks && stacks.length > 0 ? (
     <ul className="flex flex-col gap-4">
       {stacks.map((stack) => (
         <li key={stack.name}>
@@ -38,16 +64,18 @@ const ResourceGroupList = async (props: Props) => {
                     })}
                 </span>
               </Link>
-              <DeleteForm
+              {/* <DeleteForm
                 stackName={stack.name!}
                 project="resource-groups"
                 url="resource-groups"
-              />
+              /> */}
             </div>
           </ResourceRow>
         </li>
       ))}
     </ul>
+  ) : (
+    <div>No stacks</div>
   );
 };
 
