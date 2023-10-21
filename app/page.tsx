f641d8c3deff54a0b8c63c5f94d9acf19753f5b5
Tomas Jansson <tomas.jansson@gmail.com>
2023-10-21 08:08:41 +0200

    Simple message on first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <p className="flex w-full h-full justify-center items-center">
+      Pulumi automation demo. Select resource to create in the menu to the right
+    </p>
+  );
 }
